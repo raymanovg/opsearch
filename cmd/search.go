e02421b6c9676f802e5b8a45c4b531c2b4ec49f2
rayman <raymanovg@gmail.com>
2019-03-23 11:32:28 +0300

    [master] search commang and getting url arg

---
+++ cmd/search.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+)
+
+type URLFlag struct {
+	url *url.URL
+}
+
+func (f *URLFlag) String() string {
+	if f.url == nil {
+		return ""
+	}
+
+	return f.url.String()
+}
+
+func (f *URLFlag) Set(value string) error {
+	parsedURL, err := url.Parse(value)
+	if err != nil {
+		return fmt.Errorf("Cannot parse url: %v", err)
+	}
+
+	f.url = parsedURL
+
+	return nil
+}
+
+var arg URLFlag
+
+func init() {
+	flag.Var(&arg, "url", "Page url to search official page links")
+}
+
+func main() {
+	flag.Parse()
+
+	pageURL := arg.url
+	fmt.Printf("Looging for op links on page %s", pageURL.String())
+}
