ec1c8b79dbde028c7dbb69c107aaa19b49a7ff91
rayman <raymanovg@gmail.com>
2019-03-24 16:28:52 +0300

    [master] config to matcher file. Json config was deleted

--- matcher/engine/domainMatcher.go
+++ matcher/engine/domainMatcher.go
@@ -1,35 +1,22 @@
 package engine
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
-	"os"
-	"path"
-	"runtime"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/raymanovg/opsearch/matcher"
 )
 
-type (
-	domainMatcher struct{}
-	Config        struct {
-		Settings []struct {
-			Sf      string   `json:"sf"`
-			Domains []string `json:"domains"`
-		}
-	}
-)
+type domainMatcher struct{}
 
-var config Config
 var engine domainMatcher
 
 func (dm *domainMatcher) Match(document *goquery.Document) []*url.URL {
 	var links []*url.URL
-	for _, setting := range config.Settings {
-		fmt.Printf("Loking for links for %s \n", setting.Sf)
-		for _, domain := range setting.Domains {
+	for _, service := range matcher.Services {
+		fmt.Printf("Loking for links for %s \n", service.Name)
+		for _, domain := range service.Domains {
 			pattern := fmt.Sprintf("a[href*=\"%s\"]", domain)
 			document.Find(pattern).Each(func(i int, s *goquery.Selection) {
 				link, exist := s.Attr("href")
@@ -54,25 +41,7 @@ func (dm *domainMatcher) StringName() string {
 	return "domainMatcher"
 }
 
-func loadConfig() {
-	_, filename, _, _ := runtime.Caller(1)
-	configFileName := path.Join(path.Dir(filename), engine.StringName()+"Config.json")
-	file, err := os.Open(configFileName)
-	if err != nil {
-		fmt.Printf("Cannot open config file: %v", err)
-		return
-	}
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&config)
-	if err != nil {
-		fmt.Printf("Cannot decode config: %v", err)
-		return
-	}
-}
-
 func init() {
 	engine = domainMatcher{}
 	matcher.Matchers[engine.StringName()] = &engine
-	loadConfig()
 }
--- matcher/matcher.go
+++ matcher/matcher.go
@@ -6,6 +6,39 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type service struct {
+	Name    string
+	Domains []string
+}
+
+var Services = []service{
+	service{
+		Name: "Facebook",
+		Domains: []string{
+			"facebook.com",
+		},
+	},
+	service{
+		Name: "Twitter",
+		Domains: []string{
+			"twitter.com",
+		},
+	},
+	service{
+		Name: "VK",
+		Domains: []string{
+			"vk.com",
+			"vkontakte.ru",
+		},
+	},
+	service{
+		Name: "Ok",
+		Domains: []string{
+			"ok.ru",
+		},
+	},
+}
+
 var Matchers map[string]Matcher
 
 type Matcher interface {
